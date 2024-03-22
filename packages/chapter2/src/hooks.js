d7bcad173431fd7daa151cf7e2c74d1e7fe59c4b
slamdunk11 <ruret@naver.com>
2024-03-22 22:06:46 +0900

    과제 B

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,15 +1,36 @@
 export function createHooks(callback) {
+  let _val = [];
+  let currentStateKey = 0;
+  let _memoVal;
+  let _memoRefs = [];
   const useState = (initState) => {
-    return [];
+    if (_val.length === currentStateKey) {
+      _val.push(initState);
+    }
+    let state = _val[currentStateKey];
+
+    const setState = (newState) => {
+      if (newState === state) return;
+      _val[currentStateKey] = newState;
+
+      callback();
+    };
+    currentStateKey += 1;
+    return [state, setState];
   };
 
   const useMemo = (fn, refs) => {
+    if (refs.every((item, idx) => item === _memoRefs[idx])) {
+      return _memoVal;
+    }
+    _memoRefs = refs;
+    _memoVal = fn();
     return fn();
   };
 
   const resetContext = () => {
-
-  }
+    currentStateKey = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
